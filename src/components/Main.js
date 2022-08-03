d9049084a744177a44bf600d4cad02bd9fe742fc
Alexandr Yakushkin <jakushkinn@yandex.ru>
2022-08-03 15:54:33 +0300

    API_profile

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,27 @@
+import React from 'react';
+import { api } from '../utils/Api.js';
+
 export default function Main(props) {
+  api.getServerData()
+    .then(res => {
+      setUserAvatar(res.avatar);
+      setUserName(res.name);
+      setUserDescription(res.about);
+    })
+
+  const [userAvatar, setUserAvatar] = React.useState('')
+  const [userName, setUserName] = React.useState('')
+  const [userDescription, setUserDescription] = React.useState('')
   return (
     <main className="content">
       <section className="profile">
         <div className="profile__avatar" onClick={props.onEditAvatar}>
-          <img className="profile__image" alt="изображение профиля"/>
+          <img className="profile__image" alt="Изображение профиля" src={userAvatar} />
         </div>
         <div className="profile__info">
-          <h1 className="profile__name">Жак-Ив Кусто</h1>
+          <h1 className="profile__name">{userName}</h1>
           <button className="profile__edit-button" type="button" aria-label="редактирование профиля" onClick={props.onEditProfile}></button>
-          <p className="profile__job">Исследователь океана</p>
+          <p className="profile__job">{userDescription}</p>
         </div>
         <button className="profile__add-button" type="button" aria-label="добавление фотокарточки" onClick={props.onAddPlace}></button>
       </section>
@@ -16,7 +29,7 @@ export default function Main(props) {
       <section className="elements">
         <template id="card-template">
           <article className="card">
-            <img className="card__img"/>
+            <img className="card__img" />
             <h2 className="card__name"></h2>
             <div className="card__like">
               <button className="card__like-button" type="button" aria-label="поставить лайк"></button>
